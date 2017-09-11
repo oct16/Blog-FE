5aa066d9233498bf1a81a59cfba8f284e2ffaf5d
FengFan <me@fengfan.me>
2017-09-12 01:21:44 +0800

    auto deploy

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -133,10 +133,14 @@ const config = {
       },
       {
         test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
-        loader: 'file-loader',
-        query: {
-          name: isDebug ? '[path][name].[ext]?[hash:8]' : '[hash:8].[ext]',
-        },
+
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            name: isDebug ? '[path][name].[ext]?[hash:8]' : '[hash:8].[ext]',
+          },
+        }]
       },
       {
         test: /\.(mp4|webm|wav|mp3|m4a|aac|oga)(\?.*)?$/,
