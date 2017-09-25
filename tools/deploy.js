e1da06d159748e40d7e9f7aa1bb7c490a5e4f556
FengFan <me@fengfan.me>
2017-09-26 02:22:10 +0800

    auto deploy

--- tools/deploy.js
+++ tools/deploy.js
@@ -1,11 +1,6 @@
 import path from 'path';
 import { spawn } from './lib/cp';
 
-const options = {
-  cwd: path.resolve(__dirname, '../build'),
-  stdio: ['ignore', 'inherit', 'inherit'],
-};
-
 /**
  * Deploy the contents of the `/build` folder to a remote server via Git.
  */
