c7f61b22e01cf486015ca48ff79ac080c71fc89a
FengFan <me@fengfan.me>
2017-09-26 02:28:42 +0800

    auto deploy

--- tools/deploy.js
+++ tools/deploy.js
@@ -5,7 +5,8 @@ import { spawn, exec } from './lib/cp';
  * Deploy the contents of the `/build` folder to a remote server via Git.
  */
 async function deploy() {
-  await exec(process.cwd() + '/autoDeploy.sh')
+  let { stdout } = await exec(process.cwd() + '/autoDeploy.sh')
+  console.log(stdout)
 }
 
 export default deploy;
